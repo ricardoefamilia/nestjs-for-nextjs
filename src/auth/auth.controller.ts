6b70f17b43d0d5c7f76f6b552d06ae3bcbbb30b6
ricardoefamilia <ricardoefamilia@gmail.com>
2026-03-02 20:16:20 -0300

    Seção 4 - Aula 229

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {
+    return this.authService.doLogin();
+  }
+
   @Post('login')
   login() {}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  doLogin() {
+    return 'Olá do AuthService';
+  }
+}
