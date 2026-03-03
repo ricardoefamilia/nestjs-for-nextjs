a02b9025a632f6103a58fca77dab563d0e3b9377
ricardoefamilia <ricardoefamilia@gmail.com>
2026-03-03 16:27:31 -0300

    Seção 4 - Aula 232

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  doLogin() {
-    return 'Olá do AuthService';
+  doLogin(loginDto: LoginDto) {
+    console.log(loginDto.email, loginDto.password);
+    return loginDto;
   }
 }
