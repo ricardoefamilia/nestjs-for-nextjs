a05dbc158547fa86553960dc7f7781075806afd8
ricardoefamilia <ricardoefamilia@gmail.com>
2026-03-11 15:52:10 -0300

    Seção 4 - aula 251

---
+++ src/post/dto/post-response.dto.ts
@@ -0,0 +1,41 @@
+import { Post, User } from '@prisma/client';
+
+type PostWithAuthor = Post & {
+  author: Pick<User, 'id' | 'name' | 'email'>;
+};
+
+export class PostResponseDto {
+  readonly id: string;
+  readonly title: string;
+  readonly slug: string;
+  readonly content: string;
+  readonly excerpt: string;
+  readonly coverImageUrl: string;
+  readonly published: boolean;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+
+  readonly author: {
+    id: string;
+    name: string;
+    email: string;
+  };
+
+  constructor(post: PostWithAuthor) {
+    this.id = post.id;
+    this.title = post.title;
+    this.slug = post.slug;
+    this.content = post.content;
+    this.excerpt = post.excerpt;
+    this.coverImageUrl = post.coverImageUrl;
+    this.published = post.published;
+    this.createdAt = post.createdAt;
+    this.updatedAt = post.updatedAt;
+
+    this.author = {
+      id: post.author.id,
+      name: post.author.name,
+      email: post.author.email,
+    };
+  }
+}
